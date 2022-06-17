a4451d7023a5263c025e27d867928760eb166e3c
Valentina Garavaglia <vaahxx@gmail.com>
2022-06-17 19:11:04 -0400

    update .gitignore and coupon test

--- test/Coupon.test.ts
+++ test/Coupon.test.ts
@@ -5,3 +5,8 @@ test("Should create a coupon", () => {
     expect(coupon.code).toBe("COUPON_20");
     expect(coupon.discount).toBe(20);
 })
+
+test("Deve criar um cupom", function () {
+	const coupon = new Coupon("VALE20", 20);
+	expect(coupon.calculateDiscount(1000)).toBe(200);
+});
